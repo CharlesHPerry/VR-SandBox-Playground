49b9b23af53d17c286d9bc57a8938b19d7642695
charles <chuckhperry@gmail.com>
2020-08-18 18:20:41 -0700

    F*CK YEAH! I got cubes to spawn when I click a button.
    Goddamn this was a real pain to try and figure out, I wasn't able to find a clear answer anywhere on the internet so I had to peice an answer together myself.

---
+++ Assets/SpawnCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnCube : MonoBehaviour
+{
+    public GameObject sampleObject;
+    public void AddObject()
+    {
+        GameObject ObjectClone = Instantiate(sampleObject, new Vector3(2, 5, 0), Quaternion.identity);
+    }
+}
